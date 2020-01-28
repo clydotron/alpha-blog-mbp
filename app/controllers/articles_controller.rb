ed3d838346cce565e5aa0814dc26ecb65cb440e4
Brian Green <bgreen007@cox.net>
2020-01-28 04:15:27 -0600

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+
+def index
+  @articles = Article.all
+end
+
+def new
+  @article = Article.new
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def create
+  @article = Article.new(article_params)
+  if @article.save
+    flash[:success] = "Article was successfully created"
+    redirect_to article_path(@article)
+  else
+    render 'new'
+  end
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:success] = "Article was successfully updated"
+    redirect_to articles_path
+  else
+    render 'edit'
+  end
+end
+
+def show
+  @article = Article.find(params[:id])
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:danger] = "Article was successfully destroyed"
+  redirect_to articles_path
+end
+
+private
+
+def article_params
+  params.require(:article).permit(:title, :description)
+end
+
+end
